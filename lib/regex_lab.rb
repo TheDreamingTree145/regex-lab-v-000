e5d43e2057d2426b503be794f43d66a5f9589a30
Dustin Anderson <dandersonsoftware@outlook.com>
2018-06-15 18:54:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,5 +16,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/[0-9]/).length == 10 ? true : false
+!!(phone).match(/[0-9{10}]/)
 end
